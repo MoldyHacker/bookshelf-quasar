89bf80564abe1fdecc0585c8c31b4a883adc7dee
cmoldenauer <cmoldenuaer@my.wctc.edu>
2023-04-16 15:26:21 -0500

    added over some components

---
+++ src/components/Dialongs/AddBook/AddBookManualDialog.vue
@@ -0,0 +1,69 @@
+<template>
+  <q-dialog persistent v-model="value">
+    <q-card>
+      <!--header-->
+      <q-card-section class="row">
+        <span class="col-10 text-h6">Add Book Manually</span>
+        <!--            <q-btn class="col" flat rounded @click="newBook.bookmark = !newBook.bookmark" :icon="newBook.bookmark ? 'bookmark' : 'bookmark_outline' "/>-->
+        <q-checkbox
+          class="col"
+          v-model="newBook.bookmark"
+          checked-icon="bookmark"
+          unchecked-icon="bookmark_outline"
+          indeterminate-icon="help"
+          color="black"
+          size="48px"
+          dense
+        />
+      </q-card-section>
+
+      <!--Body-->
+      <q-card-section class="q-pt-none q-gutter-sm">
+        <!--Title-->
+        <q-input autofocus filled v-model="newBook.title" label="Title">
+          <template v-slot:prepend>
+            <q-icon name="menu_book" />
+          </template>
+        </q-input>
+        <!--Author-->
+        <q-input filled v-model="newBook.author" label="Author">
+          <template v-slot:prepend>
+            <q-icon name="person" />
+          </template>
+        </q-input>
+      </q-card-section>
+
+      <q-card-actions align="right" class="text-primary">
+        <q-btn flat label="Close" icon="delete" @click="resetBook" v-close-popup />
+        <q-btn flat label="Save" icon="save" @click="addBook" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  name: 'AddBookManualDialog',
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      newBook: {
+        title: '',
+        author: '',
+        bookmark: false,
+      },
+    }
+  },
+  computed: {
+    value: {
+      get() { return this.modelValue },
+      set(value) { this.$emit('update:modelValue', value)}
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Dialongs/EditBookDialog.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-dialog persistent v-model="value">
+    <q-card>
+      <q-card-section class="row">
+        <span class="col-10 text-h6">Edit Book</span>
+        <!--                <q-btn class="col" flat rounded @click="newBook.bookmarkToggle()" :icon="newBook.bookmark ? 'bookmark' : 'bookmark_outline' "/>-->
+        <q-checkbox
+          class="col"
+          v-model="book.bookmark"
+          checked-icon="bookmark"
+          unchecked-icon="bookmark_outline"
+          indeterminate-icon="help"
+          color="black"
+          size="48px"
+          dense
+        />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none q-gutter-sm">
+        <q-input autofocus filled v-model="newBook.book.title" label="Title">
+          <template v-slot:prepend>
+            <q-icon name="menu_book" />
+          </template>
+        </q-input>
+        <q-input filled v-model="newBook.book.author" label="Author">
+          <template v-slot:prepend>
+            <q-icon name="person" />
+          </template>
+        </q-input>
+      </q-card-section>
+
+      <q-card-actions align="right" class="text-primary">
+        <q-btn flat label="Close" icon="cancel" v-close-popup />
+        <q-btn flat label="Save" icon="save" @click="saveBook" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  name: 'EditBookDialog',
+  props: ['modelValue', 'book'],
+  emits: ['update:modelValue', 'save-book'],
+  date() {
+    return {
+      newBook: {...this.book}
+    }
+  },
+  computed: {
+    value: {
+      get() {
+        return this.modelValue
+      },
+      set(value) {
+        this.$emit('update:modelValue', value)
+      }
+    }
+  },
+  methods: {
+    saveBook() {
+      this.book = {...this.newBook};
+      this.$emit('save-book',this.newBook);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
